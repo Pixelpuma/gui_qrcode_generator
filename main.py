1e5857bfc36d45d5ef061c00e3b495d9156963f7
Pixelpuma <165570747+Pixelpuma@users.noreply.github.com>
2024-04-04 18:04:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import qrcode
+from tkinter import *
+
+
+def create():
+    data=inp.get()
+    name=inp2.get()
+    code=qrcode.make(data)
+    type(code)
+    code.save('./your_images/'+name+'.png')
+
+
+
+window=Tk()
+window.geometry('400x500')
+
+label=Label(text='Enter the data')
+
+
+inp=Entry(window,width=30)
+label2=Label(text='Enter a name for the qrcode')
+
+
+inp2=Entry(window,width=30)
+
+btn=Button(text='Create',command=create)
+label.pack()
+inp.pack()
+label2.pack()
+inp2.pack()
+btn.pack()
+window.mainloop()
